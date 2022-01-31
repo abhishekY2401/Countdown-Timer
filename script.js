6c9e87de15b947086b1cf525e94af19b0bf0b3e4
Abhishek Yadav <89199323+abhishekY2401@users.noreply.github.com>
2022-01-31 04:22:19 -0800

    Added a condition statement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initializing a new date for the counter to count down
-const endDate = "Dec 31, 2021 23:59:59 GMT+0530";
+const endDate = "Dec 31, 2022 23:59:59 GMT+0530";
 
 const countDown = setInterval(function UpdateTime() {
   let todayDate = new Date();
@@ -22,5 +22,10 @@ const countDown = setInterval(function UpdateTime() {
   if (diffDate <= 0) {
     clearInterval(countDown);
     document.getElementById("infoTime").innerHTML = "EXPIRED";
+    // Display the time countdown as 0 once the timeline is expired
+    document.getElementById("sec").innerHTML = 0;
+    document.getElementById("mins").innerHTML = 0;
+    document.getElementById("hrs").innerHTML = 0;
+    document.getElementById("day").innerHTML = 0;
   }
 }, 1000);
